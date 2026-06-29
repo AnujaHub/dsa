152aab5f70cd6016474a687b63da427cb8dafa35
Anuja <anuja3327@gmail.com>
2026-06-29 21:28:21 +0530

    aplha patterns

---
+++ BuildingLogic/EasyMedium/EasyMedium/pattern14.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+using namespace std; 
+
+
+// miskate i made : tried to print all A-Z 
+// new thing i learnt : 'A' + any num = 'A' , A being a  char
+int main() {
+int n = 5;
+
+for(int i=0 ; i<n ; i++){
+    for(int j=0 ; j<=i ; j++){
+        cout<<char('A'+j);
+    }
+    cout<<endl;
+}
+
+   return 0; 
+}
