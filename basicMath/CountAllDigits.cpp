70eabd649d5b16a30d830050dadcfaa20bcebc31
Anuja <anuja3327@gmail.com>
2026-06-30 20:10:48 +0530

    basicMath

---
+++ basicMath/CountAllDigits.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std; 
+ 
+int main() {
+
+int n , count = 0;
+cout<<"Enter a number:";
+cin>>n;
+
+if (n==0) {
+    count=1;
+    cout<<count;
+}
+else {
+while(n>0){
+    n=n/10;
+    count++;
+}
+
+  cout<<count;
+  return 0;
+}
+}
