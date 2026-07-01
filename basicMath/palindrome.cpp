85c52968d44d3d9dcdff5cc44b82e4feffdcd774
Anuja <anuja3327@gmail.com>
2026-07-01 21:01:28 +0530

    basicMathD2

---
+++ basicMath/palindrome.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+
+    int orgNum = 1441, reverseNum=0 , num , n = orgNum;
+
+    while(n!=0){
+        num = n%10;
+        reverseNum = ( reverseNum * 10 ) + num;
+        n = n/10;
+    }
+
+    if(reverseNum==orgNum) cout<<"Palindrome";
+
+    else cout<<"Not a Palindrome";
+
+   return 0; 
+
+}
---
+++ basicMath/reverseNum.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+
+    int n = 123, reverseNum=0 , num;
+
+    while(n!=0){
+
+        num = n%10;
+        reverseNum = ( reverseNum * 10 ) + num;
+        n = n/10;
+    }
+
+    cout<<reverseNum;
+
+   return 0; 
+
+}
