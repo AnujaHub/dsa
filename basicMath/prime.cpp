04f2d1e16c44bd084d2a08cfe9c1bab1e727c0a6
Anuja <anuja3327@gmail.com>
2026-07-03 21:27:44 +0530

    primeornot

---
+++ basicMath/prime.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std; 
+ 
+int main() {
+    int n ;
+    cout<<"enter a number";
+    cin>>n;
+
+
+    if(n<=1) cout<<"Not Prime";
+
+    else{
+        bool isPrime = true;
+        for(int i=2 ; i*i<=n ; i++){
+            if(n%i==0) {
+                isPrime = false;
+                break;
+        }
+    }
+
+    if(!isPrime) cout<<"Non Prime";
+    else cout<<"Prime";
+    }
+
+   return 0; 
+}
